6443e8df66bef81e7fecc4610ab155dd80ed6882
Adolfo de Moure <demourej@gmail.com>
2021-08-27 01:21:38 -0500

    commerce homework commit

---
+++ models/Product.js
@@ -0,0 +1,64 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model {}
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  
+  product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+  
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  
+  stock: {
+  //     Integer.
+  type: DataTypes.INTEGER,
+  // Doesn't allow null values.
+  allowNull: false,
+  // Set a default value of 10.
+  defaultValue: 10,
+  // Validates that the value is numeric.
+  validate:{
+  isInt: true, 
+  }
+    },
+    category_id: {
+      // Integer.
+  type: DataTypes.INTEGER,  
+      // References the Category model's id.
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+        },    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
